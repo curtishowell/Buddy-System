91a7b4fb6479fc874dfb1e3b4f7d6d84d2eca9e6
curtishowell <github@curtishowell.net>
2014-11-06 20:42:14 -0800

    Initial Commit

---
+++ Buddy System/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buddy System
+//
+//  Created by Curtis Howell on 11/6/14.
+//  Copyright (c) 2014 Curtis Howell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
